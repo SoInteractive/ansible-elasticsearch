1a180027fdf2f959ed1d878956baa28e3bfc25d9
Szymon Zyguła <szygula@sointeractive.pl>
2018-01-17 15:17:15 +0100

    Fixes

--- tests/test_default.py
+++ tests/test_default.py
@@ -7,23 +7,12 @@ def test_linux(SystemInfo):
     assert SystemInfo.type == 'linux'
 
 
-# def test_yum_packages(Package, SystemInfo):
-#     if SystemInfo.distribution == 'centos':
-#         present = [
-#             "python-pip"
-#         ]
-#         if present:
-#             for package in present:
-#                 p = Package(package)
-#                 assert p.is_installed
-
-
-# def test_apt_packages(Package, SystemInfo):
-#     if SystemInfo.distribution == 'ubuntu':
-#         present = [
-#             "python-pip"
-#         ]
-#         if present:
-#             for package in present:
-#                 p = Package(package)
-#                 assert p.is_installed
+def test_packages(Package, SystemInfo):
+    if SystemInfo.distribution == 'centos':
+        present = [
+            "python-pip"
+        ]
+        if present:
+            for package in present:
+                p = Package(package)
+                assert p.is_installed
